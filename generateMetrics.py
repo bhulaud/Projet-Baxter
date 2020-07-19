3f1ee63b2e710ca57d2ce4795070a8f171426220
bhulaud <bhulaud@gmail.com>
2020-07-19 23:01:49 +0200

    Create generateMetrics.py

---
+++ generateMetrics.py
@@ -0,0 +1,10 @@
+import os, sys
+import random
+
+def generateRandomMetric():
+
+  with open("metrics.txt",'w') as metricsFile:
+  
+    metricsFiles.writelines(str(random.random() + "\n"))
+    
+generateRandomMetric()
