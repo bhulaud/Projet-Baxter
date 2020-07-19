1b660a9e16d94b9417cf0ecf8f9fbd27df95907a
bhulaud <bhulaud@gmail.com>
2020-07-19 23:13:09 +0200

    Create analyseMetrics.py

---
+++ analyseMetrics.py
@@ -0,0 +1,21 @@
+import os, sys
+
+def analyseMetrics():
+
+  with open("metrics.txt",'r') as f:
+  
+    lines = f.readlines()
+    
+    with open("results.tap",'w') as resultFile :
+      
+      if int(lines[0]) > 0.3:
+      
+        line = "1..2 \n ok 1 - MOTA \n not ok 2 - MOTP # TODO: not written yet"
+
+      else:
+      
+        line = "1..2 \n not ok 1 - MOTA \n not ok 2 - MOTP # TODO: not written yet"
+
+      resultFile.writelines(line)
+      
+analyseMetrics()
